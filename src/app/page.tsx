4d6356a2c3d778d475a4f96883fa802535e0aa22
ian <iansmithxv@gmail.com>
2024-05-22 23:59:02 +0300

    Header update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-
+import React from "react";
 export default function Home() {
   return <div>Ride Share</div>;
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
 
 function Header() {
   return (
-    <div>Header</div>
-  )
+    <div
+      className="bg-white p-5 pb-3 pl-10 border-b-[4px]
+    border-gray-200 flex items-center justify-between"
+    >
+      <div className="flex gap-24 items-center">
+        <p className="font-bold text-4xl text-blue-500">NaiRideShare</p>
+      </div>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
