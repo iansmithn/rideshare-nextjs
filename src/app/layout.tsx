c31203bd281d6862f93823814f27ba7622764740
ian <iansmithxv@gmail.com>
2024-05-23 23:57:10 +0300

    Layout Update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "../components/Header";
-import React from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
 import Image from "next/image";
 import React from "react";
+
 export default function Home() {
-  return <div>Ride Share</div>;
+  return (
+    <div className="p-6 grid grid-cols-1 md:grid-cols-3 gap-5">
+      <div>Search</div>
+      <div className="col-span-2">Google Map</div>
+    </div>
+  );
 }
