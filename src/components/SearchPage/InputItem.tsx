2eedcae98076022c2c54f85bfad03a8bf9391529
ian <iansmithxv@gmail.com>
2024-05-24 23:59:29 +0300

    Search Place Autocomplete

--- src/components/SearchPage/InputItem.tsx
+++ src/components/SearchPage/InputItem.tsx
@@ -1,11 +1,22 @@
+"use client";
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
+import GooglePlacesAutocomplete from "react-google-places-autocomplete";
 
 interface InputItemProps {
   type: "source" | "destination";
 }
 
+interface Location {
+  label: string;
+  value: {
+    description: string;
+    place_id: string;
+  };
+}
+
 const InputItem: React.FC<InputItemProps> = ({ type }) => {
+  const [value, setValue] = useState<Location | null>(null);
   return (
     <div className="bg-slate-200 p-3 rounded-lg mt-3 flex items-center gap-4">
       <Image
@@ -14,10 +25,17 @@ const InputItem: React.FC<InputItemProps> = ({ type }) => {
         width={20}
         height={20}
       />
-      <input
+      {/* <input
         type="text"
         placeholder={type == "source" ? "Pickup Location" : "DropOff Location"}
         className="bg-transparent w-full outline-none"
+      /> */}
+      <GooglePlacesAutocomplete
+        apiKey={process.env.GOOGLE_API_KEY}
+        selectProps={{
+          value,
+          onChange: setValue,
+        }}
       />
     </div>
   );
