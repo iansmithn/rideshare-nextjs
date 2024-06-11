cc42db8a4e42943f14419615eb527124f8bc32ec
ian <iansmithxv@gmail.com>
2024-06-11 23:59:15 +0300

    Source/Destination Latitude & Longitude

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,26 @@
 import GoogleMapSection from "@/components/SearchPage/GoogleMapSection";
 import SearchSection from "@/components/SearchPage/SearchSection";
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
+import { SourceContext } from "../../context/Sourcecontext";
+import { DestinationContext } from "../../context/destinationContext";
 
 export default function Home() {
+  const [source, setSource] = useState([]);
+  const [destination, setDestination] = useState([]);
+
   return (
-    <div className="p-6 grid grid-cols-1 md:grid-cols-3 gap-5">
-      <div>
-        <SearchSection />
-      </div>
-      <div className="col-span-2">
-        <GoogleMapSection />
-      </div>
-    </div>
+    <SourceContext.Provider value={{ source, setSource }}>
+      <DestinationContext.Provider value={{ source, setSource }}>
+        <div className="p-6 grid grid-cols-1 md:grid-cols-3 gap-5">
+          <div>
+            <SearchSection />
+          </div>
+          <div className="col-span-2">
+            <GoogleMapSection />
+          </div>
+        </div>
+      </DestinationContext.Provider>
+    </SourceContext.Provider>
   );
 }
--- src/components/SearchPage/InputItem.tsx
+++ src/components/SearchPage/InputItem.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Image from "next/image";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import GooglePlacesAutocomplete from "react-google-places-autocomplete";
 
 interface InputItemProps {
@@ -17,6 +17,30 @@ interface Location {
 
 const InputItem: React.FC<InputItemProps> = ({ type }) => {
   const [value, setValue] = useState<Location | null>(null);
+  const [placeholder, setPlaceholder] = useState("");
+
+  useEffect(() => {
+    type == "source"
+      ? setPlaceholder("Pickup Location")
+      : setPlaceholder("DropOff Location");
+  }, []);
+
+  const getLatandLang = (place: Location, type: string) => {
+    const placeId = place.value.place_id;
+    const service = new google.maps.places.PlacesService(
+      document.createElement("div")
+    );
+    service.getDetails({ placeId }, (place, status) => {
+      if (
+        status === "OK" &&
+        place &&
+        place.geometry &&
+        place.geometry.location
+      ) {
+        console.log(place.geometry.location.lat());
+      }
+    });
+  };
   return (
     <div className="bg-slate-200 p-3 rounded-lg mt-3 flex items-center gap-4">
       <Image
@@ -25,16 +49,30 @@ const InputItem: React.FC<InputItemProps> = ({ type }) => {
         width={20}
         height={20}
       />
-      {/* <input
-        type="text"
-        placeholder={type == "source" ? "Pickup Location" : "DropOff Location"}
-        className="bg-transparent w-full outline-none"
-      /> */}
+
       <GooglePlacesAutocomplete
-        apiKey={process.env.GOOGLE_API_KEY}
+        apiKey={process.env.NEXT_PUBLIC_GOOGLE_API_KEY}
         selectProps={{
           value,
-          onChange: setValue,
+          onChange: (place) => {
+            if (place) {
+              getLatandLang(place, type);
+              setValue(place);
+            }
+          },
+          placeholder,
+          isClearable: true,
+          className: "w-full",
+          components: {
+            DropdownIndicator: null,
+          },
+          styles: {
+            control: (provided) => ({
+              ...provided,
+              backgroundColor: "#00ffff00",
+              border: "none",
+            }),
+          },
         }}
       />
     </div>
